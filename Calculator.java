8242e2783ca3b757a054a10510968701e3eadf74
szczesnym <marcins@oerlemans-foods.nl>
2018-04-05 12:10:20 +0200

    Substract and division change

--- Calculator.java
+++ Calculator.java
@@ -6,13 +6,15 @@ public class Calculator {
         return x + y;
     }
     public float divideNumbers(float x, float y) {
+        return x / y;
+    }
+    public float substractNumbers(float x, float y) {
         return x - y;
     }
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.format("SUM %f\n", calculator.addNumbers(5, 4));
         System.out.format("DIVISION %f\n", calculator.divideNumbers(5, 4));
-
-        //comment for 3rd push
+        System.out.format("SUNSTRACT %f\n", calculator.substractNumbers(5, 4));
     }
 }
